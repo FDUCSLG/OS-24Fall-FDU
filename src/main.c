2959731227e9ce0a1472c80bc3090e7fc21ea607
Boreas618 <21307130094@m.fudan.edu.cn>
2024-08-31 17:52:10 +0000

    style: convert tabs to spaces

--- src/main.c
+++ src/main.c
@@ -7,45 +7,45 @@ static volatile bool boot_secondary_cpus = false;
 
 void main()
 {
-	/**
-	 * LAB 1 TASK 1
-	 * 
-	 * Print "Hello, world!" on all four cores. The following questions may help:
-	 * (1) How to print strings.
-	 * (2) Where to place the print statements.
-	 */
+    /**
+     * LAB 1 TASK 1
+     * 
+     * Print "Hello, world!" on all four cores. The following questions may help:
+     * (1) How to print strings.
+     * (2) Where to place the print statements.
+     */
 
-	if (cpuid() == 0) {
-		/**
-		 * Core 0 is responsible for initializing the kernel.
-		 * 
-		 * The `smp_init` function wakes up the other three cores, which will
-		 * subsequently enter the `main` function and take the `else` branch below.
-		 * 
-		 * For Lab 1, carefully consider the execution path for core 0 and the other
-		 * three cores.
-		 */
-		smp_init();
+    if (cpuid() == 0) {
+        /**
+         * Core 0 is responsible for initializing the kernel.
+         * 
+         * The `smp_init` function wakes up the other three cores, which will
+         * subsequently enter the `main` function and take the `else` branch below.
+         * 
+         * For Lab 1, carefully consider the execution path for core 0 and the other
+         * three cores.
+         */
+        smp_init();
 
-		/**
-		 * Initialize the UART.
-		 * 
-		 * After initialization, the `uart_put_char` function can be used to print
-		 * characters to the screen.
-		 */
-		uart_init();
+        /**
+         * Initialize the UART.
+         * 
+         * After initialization, the `uart_put_char` function can be used to print
+         * characters to the screen.
+         */
+        uart_init();
 
-		/**
-		 * Initialize printk, which is analogous to printf.
-		 */
-		printk_init();
+        /**
+         * Initialize printk, which is analogous to printf.
+         */
+        printk_init();
 
-		// Set a flag indicating that the secondary CPUs can start executing.
-		boot_secondary_cpus = true;
-	} else {
-		while (!boot_secondary_cpus)
-			;
-	}
+        // Set a flag indicating that the secondary CPUs can start executing.
+        boot_secondary_cpus = true;
+    } else {
+        while (!boot_secondary_cpus)
+            ;
+    }
 
-	set_return_addr(idle_entry);
+    set_return_addr(idle_entry);
 }
